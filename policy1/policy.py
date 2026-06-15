56c64d07120485b5fa6592187aa05f4b4f3324c4
Yadavzi <79758696+AbhishektheGoat@users.noreply.github.com>
2026-06-15 13:23:57 +0530

    Update policy.py

--- policy1/policy.py
+++ policy1/policy.py
@@ -1,11 +1,14 @@
 import streamlit as st
+import os
 
-with open("Policies.pdf", "rb") as pdf_file:
+pdf_path = os.path.join(os.path.dirname(__file__), "Policies.pdf")
+
+with open(pdf_path, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 
 st.download_button(
     label="Download PDF",
     data=PDFbyte,
-    file_name="sample.pdf",
+    file_name="Policies.pdf",
     mime="application/pdf"
 )
